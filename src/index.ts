c3689d56f6fe7af8f7cc6eed8b3d5becfaa0f317
Daniel Huisman <daniel@huisman.me>
2020-08-11 22:31:44 +0200

    Change orbis-server to @orbis-framework/core

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-// import {Orbis} from 'orbis-server';
+import {Orbis} from '@orbis-framework/core';
 import {auth} from '@orbis-framework/auth';
 
-// const orbis = new Orbis();
+const orbis = new Orbis();
 
+console.log(orbis);
 console.log(auth);
