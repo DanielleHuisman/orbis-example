5a64c6106a32dba9dbe7bcd94d35ec8f8a9ff810
Daniel Huisman <daniel@huisman.me>
2020-09-02 21:51:08 +0200

    Update to new provider options system

--- src/orbis.ts
+++ src/orbis.ts
@@ -1,9 +1,7 @@
 import {Orbis} from '@orbis-framework/core';
 import {OrbisAuth} from '@orbis-framework/auth';
-import {oauth2_v2} from 'googleapis';
 
 import {config} from './config';
-import {User} from './entities';
 import {providers} from './providers';
 
 export const orbis = new Orbis();
@@ -14,32 +12,6 @@ orbis.addModules([
         jwt: config.jwt,
         urls: {
             prefix: (url) => url.startsWith('/') ? `${config.externalUrl}${url}` : config.externalUrl
-        },
-        // TODO: this should be moved to each provider separated
-        createUser: async ({provider, data}) => {
-            if (provider.type === 'local') {
-                const localData = data as {
-                    name: string;
-                };
-
-                return orbis.createOne(User, {
-                    data: {
-                        name: localData.name,
-                        email: provider.email
-                    }
-                });
-            } else if (provider.type === 'google') {
-                const googleData = data as oauth2_v2.Schema$Userinfo;
-
-                return orbis.createOne(User, {
-                    data: {
-                        name: googleData.name,
-                        email: provider.email
-                    }
-                });
-            } else {
-                throw new Error('Unknown provider');
-            }
         }
     })
 ]);
--- src/providers.ts
+++ src/providers.ts
@@ -1,14 +1,59 @@
 import {ProviderLocal, ProviderGoogle} from '@orbis-framework/auth';
 
 import {config} from './config';
+import {User} from './entities';
+import {orbis} from './orbis';
+
+export interface ExtendedRegisterInput {
+    name: string;
+}
 
 export const providers = [
-    new ProviderLocal({
+    new ProviderLocal<ExtendedRegisterInput>({
         extendRegisterInput(t) {
             t.string('name', {
                 nullable: false
             })
+        },
+
+        createUser({provider, data}) {
+            const localData = data as {
+                name: string;
+            };
+
+            return orbis.createOne(User, {
+                data: {
+                    name: localData.name,
+                    email: provider.email
+                }
+            });
+        },
+
+        async onEmailUpdated(provider) {
+            // Find user
+            const user = (await provider.user) as User;
+
+            // Update user email address
+            await orbis.updateOne(User, {
+                where: {
+                    id: user.id
+                },
+                data: {
+                    email: provider.email
+                }
+            });
         }
     }),
-    new ProviderGoogle(config.oauth.google)
+    new ProviderGoogle({
+        ...config.oauth.google,
+
+        createUser({provider, data}) {
+            return orbis.createOne(User, {
+                data: {
+                    name: data.name,
+                    email: provider.email
+                }
+            });
+        }
+    })
 ];
